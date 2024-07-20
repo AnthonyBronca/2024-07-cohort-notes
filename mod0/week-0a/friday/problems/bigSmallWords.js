48592bb3e53474157b28bd57fc67c6131c80895a
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-07-19 20:57:21 -0400

    added friday notes

--- mod0/week-0a/friday/problems/bigSmallWords.js
+++ mod0/week-0a/friday/problems/bigSmallWords.js
@@ -1,7 +1,8 @@
 /*
 
 Write a function called bigLittleWords. it will take in two words. The first word will
-be capitalized. The second word will be lowerCased. The function will return the phrase of combining these two words, space seperated
+be everything will be capitalized. The second word will be lower cased. The function will return the phrase of combining
+these two words, space seperated
 
 example:
 
@@ -9,6 +10,20 @@ word1 -> sam
 word2 -> AntHONy
 
 answer should be: "SAM anthony"
+
+
+1. UNDERSTAND
+Take in 2 words, the first is lowercased. the 2nd is uppercased.
+Return the two words together, make sure they are seperated by spaces
+
+2. PSUEDOCODE
+
+1. Write a function called bigLittleWords. ✅
+    - the function should take in 2 words
+        - the first word needs to be uppercased
+        - the second word needs to be lowercased
+2. Combine the words, we can use interpolation to get an output ✅
+3. return the combination
 
 
 */
---
+++ mod0/week-0a/friday/scratch.js
@@ -0,0 +1,82 @@
+/*
+
+function hello(){
+    return "Hello"
+}
+
+
+// function invocation
+
+hello()
+
+*/
+
+// function doubler(num1){
+//     return num1 * 2;
+// }
+
+// console.log(doubler(5)); // function call. it gets an output
+// doubler(6); // function call. it gets an output
+
+
+
+
+// let anthony = "anthony";
+
+
+// console.log(helloWorld.toUpperCase());;
+
+// console.log(`anthony's dad was saying, " ${anthony.toUpperCase()}!!!!!!"`)
+
+
+
+
+/*
+
+making a peanut and jelly sandwich:
+
+1. grab bread from the pantry
+2. add peanutbutter to one slice
+3. add jelly to the other slice
+4. put them together
+5. enjoy
+
+POLYAS
+
+1. UNDERSTAND THE PROBLEM
+
+- We need to grab the peanutbutter from the pantry. We need to put it on the counter
+- we need to take out 2 slices of bread per sandwich
+- we need to find the peanutbutter jar
+- we need to find the jelly jar
+- we need a knife (maybe 2)
+- open the peanut jar and put peanut butter
+- open the jelly jar and put jelly
+- smear both of these well
+- smash sandwiches together
+- take a bite
+- profit
+
+
+2.
+
+1. write a function called makePbJ✅
+    - Function should take in pb -> string✅
+    - function should take in jelly -> string✅
+2. the function should return a string that says "peanubutter and jelly sandwich"
+3. save the string in a variable called pbj NOTE: use interpolation with pb, and jelly
+4. return it
+
+*NOTE* make sure to invoke the function with pbj variables
+
+
+
+
+*/
+
+// function makePbJ(pb, jelly){
+//     let pbj = `${pb} and ${jelly} sandwich`;
+//     return pbj;
+// }
+
+// console.log(makePbJ("Peanut butter", "Jelly"));
