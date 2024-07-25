b45f0e96aae4d1e0665fcbaaa521c3e421e5b5f2
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-07-24 22:30:11 -0400

    added scratch

---
+++ mod0/week-0b/wed/lecture2/scratch.js
@@ -0,0 +1,60 @@
+// let makeCakeSteps = [
+//     "Get ingredients",
+//     "Mix Cake Flour",
+//     "Beat egg whites",
+//     "Add Sugar",
+//     "Add Flour Mix",
+//     "Place mixture in Tube Cake Pan",
+//     "Bake at 325 for 50 minutes",
+//     "Turn Cooked cake upside down",
+//     "Sift over Cake",
+//     "Slice and Enjoy",
+// ];
+
+
+// console.log(makeCakeSteps[makeCakeSteps.length - 1])
+
+
+
+// let arr = ["a", "b", "c"];
+// console.log(arr.join(""))
+
+// console.log(arr.includes("a") && arr.includes("A"))
+
+
+
+// let mySentence = "Hello how are you all doing today";
+
+// function countWords(sentence){
+//     let strArr = sentence.split(" ");
+//     return strArr.length;
+// }
+
+// console.log(countWords(mySentence))
+
+
+
+// let arr = ["a", "b", "c"];
+
+// arr[1] = "d"
+
+// console.log(arr)
+
+
+
+// let str = "abc";
+
+// str[1] = "d"
+
+// console.log(str);
+
+
+// let vowels = "aeiou";
+
+// let arr = ["a", "b", "c"];
+
+// if(vowels.includes(arr[2])){
+//     console.log("we found a vowel!")
+// } else{
+//     console.log("we did not find a vowel!")
+// }
