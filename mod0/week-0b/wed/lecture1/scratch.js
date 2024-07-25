d6311a4826ce840e73f4aa7561e4232014cd10f2
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-07-24 20:25:55 -0400

    added scratch

--- mod0/week-0b/wed/lecture1/scratch.js
+++ mod0/week-0b/wed/lecture1/scratch.js
@@ -1,4 +1,81 @@
-let str1 = 'abc';
-let str2 = 'abc';
+// const makeCakeSteps = [
+//     "Get ingredients",
+//     "Mix Cake Flour",
+//     "Beat egg whites",
+//     "Add Sugar",
+//     "Add Flour Mix",
+//     "Place mixture in Tube Cake Pan",
+//     "Bake at 325 for 50 minutes",
+//     "Turn Cooked cake upside down",
+//     "Sift over Cake",
+//     "Slice and Enjoy",
+// ];
 
-console.log(str1 === str2)
+// let arr = [1,2,3]
+// let arr2 = ["a","b","c", "d", "f", "g"]
+
+
+
+
+// /* JOIN */
+
+// let str = arr2.join("");
+// console.log(str)
+
+
+
+// if(arr2.includes("j")){
+//     console.log("we found an j")
+// } else{
+//     console.log("we did not find an j")
+// }
+
+
+
+
+
+/* INCLUDES */
+
+// console.log(arr2.includes("A"))
+
+
+
+
+// console.log(arr2[0]);
+
+// console.log(arr2.length)
+
+
+// console.log(arr2[arr2.length - 1])
+// console.log(makeCakeSteps)
+// console.log(arr, arr2)
+
+
+// let mySentence = "Hello how are you all doing today";
+
+// // console.log(sentence.length);
+
+// function countWords(sentence){
+
+//     let strArr = sentence.split(" ");
+//     return strArr.length;
+
+// }
+
+
+// console.log(countWords(mySentence));
+
+
+
+let arr = ["a", "b", "c"];
+
+arr[1] = "d";
+
+
+console.log(arr);
+
+
+let str = "abc";
+str[1] = "d"
+
+console.log(str)
