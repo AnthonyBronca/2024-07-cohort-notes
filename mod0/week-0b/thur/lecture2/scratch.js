9550a754297eaceb0e5ee8226b4654448bbc623b
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-07-25 22:48:03 -0400

    Added scratch for lecture 2

---
+++ mod0/week-0b/thur/lecture2/scratch.js
@@ -0,0 +1,133 @@
+
+
+// let arr = ["a","b","c"];
+
+
+
+
+
+
+
+// let idk = arr.pop();
+
+// console.log(arr)
+// console.log(arr.length)
+// console.log(idk)
+
+
+
+// let idk = arr.push("d","f", "g", "h");
+
+// arr[arr.length - 1] = undefined;
+// arr.length = arr.length - 1
+// // console.log(arr[arr.length - 1]);
+
+
+
+// console.log(arr);
+// console.log(arr.length)
+// console.log(idk);
+
+
+// arr[0] = "d"
+// arr[3] = "d"
+// console.log(arr)
+// console.log(arr.length)
+
+// console.log(arr[arr.length]);
+
+
+
+
+
+// let vegetableList = ["onion", "carrot", "avacado"];
+// let fruitList = ["apple", "banana", "peach"];
+
+// // step 1: remove the tomato from the back of the veggie list
+// let lastEl = vegetableList.pop();
+// fruitList.push(lastEl)
+// vegetableList.push("potato")
+
+
+// // step 2: add the tomato to the back of the fruit list
+
+
+// console.log(vegetableList);
+// console.log(fruitList)
+
+
+
+/*
+
+let arr = ["a","b","c"];
+["b", "c"];
+
+a
+
+
+*/
+
+// let arr = ["a","b","c"];
+// let idk = arr.shift();
+// console.log(arr)
+// console.log(idk)
+
+// arr[0] = null;
+// arr.length--
+
+
+// let idk = arr.unshift("d");
+// console.
+// log(arr)
+// console.log(idk);
+
+
+
+// let people1 = ["anthony", "sam", "ed"];
+// let people2 = ["alexi", "zaviar", "gabe"];
+
+
+// // 1. remove ed
+// let ed = people1.pop();
+// // 2. remove sam
+// let sam = people1.pop();
+
+// // 3. remove alexi
+// let alexi = people2.shift();
+// people1.push(alexi);
+
+// // 4. add sam to the back of people2
+
+// people2.push(sam)
+
+// console.log(ed);
+// console.log(sam)
+
+
+// console.log(people1);
+// console.log(people2);
+
+
+// let edwardOnlyLobby = [];
+
+// for(let i = 0; i < 5; i++){
+//     edwardOnlyLobby.push(ed)
+// }
+
+// console.log(edwardOnlyLobby)
+
+
+
+let firstName = 1;
+
+let arr = [`${firstName}`];
+
+console.log(arr)
+
+
+firstName = "anthony"
+
+arr.push(firstName)
+
+console.log(arr)
+// arr.push(firstName)
