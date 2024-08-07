97b2b85d01a896d2d9eb2633ad577756fde4be2a
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-08-07 19:10:58 -0400

    added problem 5 and 6

---
+++ mod1/week-02/03-wed/problems/06-removeFromFront.js
@@ -0,0 +1,19 @@
+/*
+
+Write a function called removeFromFront that will take an array and remove the element from the front of the array.
+The function should return the modified array
+
+*/
+
+
+
+
+
+
+let arr1 = ["a", "b", "c", "d"];
+let arr2 = ["anthony", "sam", "edward", "gabe"];
+let arr3 = [1, 2, 3];
+
+console.log(removeFromFront(arr1)); // ["b", "c", "d"];
+console.log(removeFromFront(arr2)); // ["sam", "edward", "gabe"];
+console.log(removeFromFront(arr3)); // [2 , 3];
