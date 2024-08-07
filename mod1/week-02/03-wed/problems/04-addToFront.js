1f34dcfea8b15c36a432a43dca246aba4f41a216
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-08-07 19:04:16 -0400

    added problem 4

---
+++ mod1/week-02/03-wed/problems/04-addToFront.js
@@ -0,0 +1,19 @@
+/*
+
+Write a function called addToFront that will take an array and an element and add the element to the front of the array.
+The function should return the modified array
+
+*/
+
+
+
+
+
+
+let arr1 = ["a", "b", "c", "d"];
+let arr2 = ["anthony", "sam", "edward", "gabe"];
+let arr3 = [1, 2, 3];
+
+console.log(addToBack(arr1, "e")); // ["e", "a", "b", "c", "d"];
+console.log(addToBack(arr2, "alexi")); // ["alexi", "anthony", "sam", "edward", "gabe"];
+console.log(addToBack(arr3, 4)); // [4, 1, 2, 3];
