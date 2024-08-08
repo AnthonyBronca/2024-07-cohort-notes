e2771898117324333c8779f8ed294a5142d8f541
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-08-07 20:35:27 -0400

    added scratch

---
+++ mod1/week-02/03-wed/lecture1/scratch.js
@@ -0,0 +1,84 @@
+
+// function mySlice(arr, startingPoint, endingPoint){
+//     let copy = [];
+//     for(let i = startingPoint; i < endingPoint; i++){
+//         let el = arr[i];
+//         copy.push(el);
+//     }
+
+//     return copy;
+// }
+
+
+//          0    1    2    3
+// let arr = ["A", "B", "C", "D"];
+
+// console.log(arr.slice(1,3).join("-"));
+
+
+// let arr2 = mySlice(arr, 1, 2)
+// console.log(arr2)
+
+
+// let arr2 = arr.slice(1, -2)
+// console.log(arr2)
+
+
+
+// console.log(arr, "This is the original");
+// console.log(arr2, "this is the copy");
+
+
+
+
+// 1st argument -> starting point (inclusive)
+// 2nd argument -> number of items to delete (use 0 if dont want to delete)
+// 3rd and above argument (optional) -> any item you want to add
+// arr.splice(1, 3, "E");
+
+// console.log(arr)
+
+
+
+// console.log(arr.indexOf("H")); // tells us where an element exists if it is found, or -1 if not
+
+
+// console.log(arr.includes("a")) // does it exist inside the array
+
+
+
+// remove from the front
+// arr.shift();
+// console.log(arr)
+
+
+
+// remove from the back
+// arr.pop();
+// console.log(arr)
+
+
+
+// add to the front
+// arr.unshift("E");
+// console.log(arr)
+
+
+
+// add to the back
+
+// arr.push("E");
+// console.log(arr)
+
+
+
+
+
+
+
+// console.table(arr)
+
+
+// let math = arr.length - 1;
+
+// console.log(arr[math])
