92c21acf4e692799d3895f6bdeaca9d4e0a4a96e
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-08-12 20:48:50 -0400

    removed solution

---
+++ mod1/week-03/lecture1/scratch.js
@@ -0,0 +1,70 @@
+
+
+
+// const firstName = "anthony";
+// firstName = "sam"
+
+
+// // let firstName = "anthony";
+
+// // firstName = [1,2,3]
+
+
+// console.log(firstName)
+
+
+
+// const colors = ["red", "white", "blue"];
+
+// function checkIfColorIsInColors(color){
+//     return colors.includes(color);
+
+// }
+
+
+// colors.push("yellow");
+
+// console.log(checkIfColorIsInColors("red")); // true;
+// console.log(checkIfColorIsInColors("yellow")); // false;
+// console.log(checkIfColorIsInColors("blue")); // true;
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+const colors = ["red", "white", "blue"];
+
+
+// function checkIfColorIsInColors(color){
+//     return colors.includes(color);
+
+// }
+
+
+
+// const checkIfColorIsInColors = color => colors.includes(color) ? 123 : color
+//     // if(colors.includes(color)){
+//     //     return true;
+//     // } else{
+//     //     return false;
+//     // }
+// //                             true      |   false
+
+// // const checkIfColorIsInColors = (color) => colors.includes(color);
+
+// console.log(checkIfColorIsInColors("red")); // true;
+
+
+// 1. whale
