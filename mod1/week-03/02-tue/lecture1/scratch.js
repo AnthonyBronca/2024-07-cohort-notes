942589335f2b66d13c518929c2ba4ec1dbe04a2b
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-08-13 20:40:22 -0400

    added scratch

--- mod1/week-03/02-tue/lecture1/scratch.js
+++ mod1/week-03/02-tue/lecture1/scratch.js
@@ -1,16 +1,84 @@
-const myMovieTheatre = {
-    theatreName: "Anthony's Movie Theatre",
-    phoneNumber: 123456789,
-    totalOccupancy: 200,
-    address: "123 SomeStreet Way.",
-    zipcode: 55555,
-    city: "Miami",
-    state: "Florida",
 
+
+const myMovieTheatre = {
+    name: "Anthony's Awesome Movie Theare",
+    occupancy: 200,
+    movie: ["Deadpool 3", "Avengers: Game", "Despicable Me"],
+
+};
+
+
+// LOOPS
+
+// - for...in
+
+let vals = []
+
+for(let key in myMovieTheatre){
+    let val = myMovieTheatre[key];
 }
 
+console.log(vals)
 
-console.log(myMovieTheatre);
-console.log(Object.keys(myMovieTheatre));
-console.log(Object.values(myMovieTheatre));
-console.log(Object.entries(myMovieTheatre));
+
+// - Object.keys
+// console.log(Object.keys(myMovieTheatre));
+// - Object.values
+// console.log(Object.values(myMovieTheatre));
+// - Object.entries
+// console.log(Object.entries(myMovieTheatre));
+
+
+
+
+
+
+
+
+
+
+
+// console.log(myMovieTheatre.student)
+
+// creates a key value pair
+// myMovieTheatre.student = "Cayden"
+
+// console.log(myMovieTheatre.student)
+
+
+
+// let myVariable = "movie"
+
+// console.log(myMovieTheatre[myVariable]);
+// dot notation can not use variables
+// console.log(myMovieTheatre.movie);
+
+
+
+
+
+
+
+
+
+
+
+
+// const crypto = {
+//     name: "Crypto",
+//     abilities: {
+//        cloak: {
+//         description: "allows you to cloak and go invisible",
+//         duration: 3
+//        },
+//        drone: {
+//         description: "can throw a drone to do surveillance",
+//         duration: 10
+//        }
+//     },
+// };
+
+
+
+// // console.log(crypto["name"]); // Crypto
+// console.table(crypto);
