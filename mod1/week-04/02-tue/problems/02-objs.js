5beda930bfa2b4808ef24213ed4583916ec1457e
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-08-20 19:33:00 -0400

    added problems

---
+++ mod1/week-04/02-tue/problems/02-objs.js
@@ -0,0 +1,25 @@
+/*
+
+Write a function that will go through an array of fruit and count each fruit.
+Return an object that has the count of each fruit
+
+
+*/
+
+const countFruits = (fruits) => {
+
+};
+
+
+const fruits = ["apple", "banana", "watermelon", "strawberry", "apple", "apple", "banana", "strawberry"];
+console.log(countFruits(fruits));
+
+/*
+{
+    apple: 3,
+    banana: 2,
+    watermelon: 1
+    strawberry: 2
+}
+
+*/
---
+++ mod1/week-04/02-tue/problems/03-debug.js
@@ -0,0 +1,14 @@
+
+
+/*
+
+You are given a function but it is broken! Try to fix it
+
+*/
+
+
+const someFunction = (nums) => nums.forEach((num) => num * 2);
+
+
+
+console.log(someFunction([1,2,3])); // [2,4,6]
