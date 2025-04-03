a131846eed0471f426f3f800a65c46327ebdd036
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2025-04-02 23:26:01 -0400

    added notes

---
+++ mod6/week-33/03-wed/lecture2/scratch.js
@@ -0,0 +1,58 @@
+
+
+// hello this is a single line comment
+
+/*
+
+hello this
+is
+a multi line
+comment
+
+*/
+
+
+// console.log("Hello world");
+
+
+// if(true){
+//     var name = "ant"
+// }
+
+// var name = "bob"
+// console.log(name)
+
+
+// const name = "ant"
+
+// var name = "ant"
+
+
+// let myNameIs = "anthony";
+
+// console.log(2 === "2");
+
+
+// function sayName(name){
+
+// }
+
+
+// sayName("anthony")
+
+
+
+let name = "janea";
+
+// console.log(name.length)
+
+
+console.log(Object.getOwnPropertyDescriptors(name));
+
+// if(name === "gary"){
+//     console.log("Hi I am gary")
+// } else if (name === "jane"){
+//     console.log("Hi I am jane")
+// } else {
+//     console.log("I may or may not be bob");
+// }
---
+++ mod6/week-33/03-wed/lecture2/scratch.py
@@ -0,0 +1,48 @@
+# hello this is a single line comment
+
+"""
+
+aw,dmawkldj
+awadawdkmawfdkm
+dawdawd
+
+"""
+
+# print("Hello World")
+
+
+# my_name_is = "Anthony"
+
+# l1 = "2"
+
+# print(l1)
+
+
+# n = 5
+# x = 5
+
+# print(n == x)
+
+
+# def say_name(name):
+#     print(name)
+
+# say_name("anthony")
+
+
+name = "bob"
+
+print(len(name))
+
+
+# if name == "gary":
+#     print("Hi I am gary")
+# elif name == "jane":
+#     print("Hi I am jane")
+# else:
+#     print("I may or may not be bob")
+
+
+boolean = True
+boolean2 = False
+nothing = None
