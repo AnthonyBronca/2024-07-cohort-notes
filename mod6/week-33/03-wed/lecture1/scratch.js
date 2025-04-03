01947b01187412eecf41036bfe89f6fc436d759f
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2025-04-02 21:12:10 -0400

    Added hw

---
+++ mod6/week-33/03-wed/lecture1/scratch.js
@@ -0,0 +1,63 @@
+
+// this is a single line comment
+/*
+
+
+kawmdklamd
+wd
+awdaw
+d
+awf
+awd
+multi line
+comment
+
+*/
+
+
+// if(true){
+//     var myName = "anthony"
+// }
+
+// console.log(myName)
+
+// function sayHi(name){
+//     console.log("say hi ", name);
+// }
+
+// sayHi("anthony");
+
+// console.log(3 === "3");
+
+// let myBool = true;
+// let myNull = null;
+
+
+// let myVariable;
+
+// console.log(myVariable)
+
+
+// function returnNothing(){
+
+// }
+
+
+let name = "gary";
+
+// if(name === "jane"){
+//     console.log("hi from jane")
+// } else if (name === "gary"){
+//     console.log("hi from gary")
+// } else {
+//     console.log("I am probably bob, but it is not confirmed")
+// }
+
+
+console.log(name.length)
+
+try{
+    console.log(name[100])
+}catch {
+    console.log(2)
+}
---
+++ mod6/week-33/03-wed/lecture1/scratch.py
@@ -0,0 +1,56 @@
+# this is a single line comment
+
+"""
+this is a muli line comment
+"""
+
+# my_name = "hello world"
+
+
+# print(my_name)
+
+
+# def say_name(name):
+#     print(name)
+
+
+# say_name("Anthony")
+
+
+# sum = 0
+
+# n = 3
+# y = 5
+
+# print(n == "3")
+
+# string = ""
+
+# n = 3
+
+# my_bool = True
+
+# my_false = False
+
+
+# my_null = None
+
+
+# # def return_nothing():
+# #     pass
+
+# def return_none():
+#     return None
+
+
+# my_null = 3
+
+name = "jane"
+
+
+# if name == "jane":
+#     print("Hi from jane")
+# elif name == "gary":
+#     print("Hi from gary")
+# else:
+#     print("I am probably bob, but it is not confirmed")
