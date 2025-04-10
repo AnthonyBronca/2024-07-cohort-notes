82237f7ce24af03f08699108a6f0f5e7757f1bd5
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2025-04-09 20:08:38 -0400

    added template

---
+++ mod6/week-34/03-wed/template_app/__init__.py
@@ -0,0 +1,27 @@
+# Imports
+
+
+anthony = User("Anthony")
+bank1 = Bank(100)
+anthony.bank = bank1
+print(anthony)
+# bank1.change_pin()
+# print(bank1.pin)
+# bank1.withdraw(7)
+
+target = Store("Target")
+cart1 = Cart()
+target.get_store()
+print("\n", "\n", "----------------", "\n", "\n")
+cart1.add_to_cart("apples", 3),
+cart1.add_to_cart("bananas", 5),
+cart1.add_to_cart("bread", 4),
+cart1.add_to_cart("eggs", 1),
+
+print(cart1)
+target.transaction(bank1, cart1.cart)
+
+print("\n", "\n", "----------------", "\n", "\n")
+target.get_store()
+print("\n", "\n", "----------------", "\n", "\n")
+print(anthony)
